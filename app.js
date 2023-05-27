a320e979d649ec871acd45c7def0b62827128cb4
JEEVAABI <jeevaabishake@gmail.com>
2023-05-27 10:40:29 +0530

    Add. rest route operations.

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("this is initial")
+const express = require('express')
+const app = express()
+const PORT =3500
+
+const todo_items = require('./routes/todo_items')
+app.get('/',(request, response) => {
+    response.send('working')
+})
+
+app.use('api/v1//todo_items', todo_items)
+
+ 
+
+app.listen(PORT)
---
+++ routes/todo_items.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/',(request, response) => {
+    response.send('list of all to-do items')
+})
+
+
+module.exports = router
